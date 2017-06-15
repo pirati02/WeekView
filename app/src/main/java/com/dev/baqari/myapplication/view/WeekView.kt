83856abd075d7ef600695b58d0203974a272e4f8
pirati02 <baqari20@gmail.com>
2017-06-16 01:31:45 +0400

    rounded week items

--- app/src/main/java/com/dev/baqari/myapplication/view/WeekView.kt
+++ app/src/main/java/com/dev/baqari/myapplication/view/WeekView.kt
@@ -1,9 +1,7 @@
 package com.dev.baqari.myapplication.view
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.support.v4.view.GestureDetectorCompat
 import android.util.AttributeSet
 import android.util.TypedValue
@@ -11,6 +9,7 @@ import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
 import com.dev.baqari.myapplication.R
+
 
 class WeekView(context: Context, attrs: AttributeSet?) : View(context, attrs), GestureDetector.OnGestureListener {
     private var mWeekItemHeight = 0
@@ -41,12 +40,12 @@ class WeekView(context: Context, attrs: AttributeSet?) : View(context, attrs), G
 
     init {
         mWeekItemPaint.color = Color.parseColor("#e4e4e4")
-        mWeekHourTextPaint.textSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 15f, resources.displayMetrics)
+        mWeekHourTextPaint.textSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 14f, resources.displayMetrics)
         mWeekHourTextPaint.color = Color.BLACK
         mTouchItemPaint.color = Color.RED
         mTouchTextPaint.color = Color.WHITE
-        mTouchTextPaint.textSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 16f, resources.displayMetrics)
-        mWeekItemHeight = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 90f, resources.displayMetrics).toInt()
+        mTouchTextPaint.textSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 14f, resources.displayMetrics)
+        mWeekItemHeight = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 70f, resources.displayMetrics).toInt()
     }
 
     override fun onDraw(canvas: Canvas?) {
@@ -59,30 +58,32 @@ class WeekView(context: Context, attrs: AttributeSet?) : View(context, attrs), G
             val left = Padding.LEFT.value.toDip()
             if (i == 0 && top > 15) {
                 mScrollY = 15f
-                canvas?.drawRect(left, ((it * mWeekItemHeight) + 20f), right, (((it * mWeekItemHeight) + mWeekItemHeight + 15f)), mWeekItemPaint)
-                canvas?.drawText(it.toHour(), (left / 5), ((it * mWeekItemHeight) + 15f) + 20f, mWeekHourTextPaint)
+                val rect = Rect(left.toInt(), (((it * mWeekItemHeight) + 20f).toInt()), right.toInt(), ((((it * mWeekItemHeight) + mWeekItemHeight + 15f).toInt())))
+                canvas?.drawPath(rect.toPath(10f), mWeekItemPaint)
+                canvas?.drawText(it.toHour(), (left / 5), ((it * mWeekItemHeight) + 40f), mWeekHourTextPaint)
                 onTouchEvent(null)
             } else if (i == 23 && bottom < mDisplayHeight) {
                 mBottomOverlayed = false
                 bottom = (((it * mWeekItemHeight) + mWeekItemHeight - 20.toDip()))
 
-                canvas?.drawRect(left, top, right, bottom, mWeekItemPaint)
-                canvas?.drawText(it.toHour(), (left / 5), top + 15f, mWeekHourTextPaint)
+                val rect = Rect(left.toInt(), top.toInt(), right.toInt(), bottom.toInt())
+                canvas?.drawPath(rect.toPath(10f), mWeekItemPaint)
+                canvas?.drawText(it.toHour(), (left / 5), top + 20f, mWeekHourTextPaint)
                 onTouchEvent(null)
             } else {
-                canvas?.drawRect(left, top, right, bottom, mWeekItemPaint)
+                val rect = Rect(left.toInt(), top.toInt(), right.toInt(), bottom.toInt())
+                canvas?.drawPath(rect.toPath(10f), mWeekItemPaint)
                 canvas?.drawText(it.toHour(), (left / 5), top + 15f, mWeekHourTextPaint)
             }
 
             mLocationMap.add(top)
         }
         if (mOnLongPressed) {
-            canvas?.drawRect(
-                    Padding.LEFT.value.toDip(),
-                    mTouchItem.top - 5,
-                    (mDisplayWidth - Padding.RIGHT.value.toDip()),
-                    mTouchItem.bottom - 5,
-                    mTouchItemPaint)
+            val rect = Rect(Padding.LEFT.value.toDip().toInt(),
+                    (mTouchItem.top).toInt(),
+                    ((mDisplayWidth - Padding.RIGHT.value.toDip()).toInt()),
+                    (mTouchItem.bottom - 5).toInt())
+            canvas?.drawPath(rect.toPath(10f), mTouchItemPaint)
             canvas?.drawText(resources.getString(R.string.new_event) + " +", left + 70.toDip(), mTouchItem.top + 30, mTouchTextPaint)
             mOnLongPressed = false
         }
@@ -141,11 +142,11 @@ class WeekView(context: Context, attrs: AttributeSet?) : View(context, attrs), G
     }
 
     override fun onDown(p0: MotionEvent?): Boolean {
-        return true
+        return false
     }
 
     override fun onFling(p0: MotionEvent?, p1: MotionEvent?, p2: Float, p3: Float): Boolean {
-        return true
+        return false
     }
 
     override fun onScroll(event: MotionEvent?, event1: MotionEvent?, p2: Float, p3: Float): Boolean {
@@ -160,6 +161,23 @@ class WeekView(context: Context, attrs: AttributeSet?) : View(context, attrs), G
             return false
         }
     }
+}
+
+fun Rect.toPath(radius: Float): Path {
+    val path = Path()
+
+    path.moveTo(this.left + radius / 2, this.top.toFloat())
+    path.lineTo(this.right - radius / 2, this.top.toFloat())
+    path.quadTo(this.right.toFloat(), this.top.toFloat(), this.right.toFloat(), this.top + radius / 2)
+    path.lineTo(this.right.toFloat(), this.bottom - radius / 2)
+    path.quadTo(this.right.toFloat(), this.bottom.toFloat(), this.right - radius / 2, this.bottom.toFloat())
+    path.lineTo(this.left + radius / 2, this.bottom.toFloat())
+    path.quadTo(this.left.toFloat(), this.bottom.toFloat(), this.left.toFloat(), this.bottom - radius / 2)
+    path.lineTo(this.left.toFloat(), this.top + radius / 2)
+    path.quadTo(this.left.toFloat(), this.top.toFloat(), this.left + radius / 2, this.top.toFloat())
+    path.close()
+
+    return path
 }
 
 
